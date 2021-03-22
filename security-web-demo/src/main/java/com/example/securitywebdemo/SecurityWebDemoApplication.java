6984495a8494df53c7f0fdf9a2b6aa8078110e35
beginor <beginor@qq.com>
2021-03-22 17:33:03 +0800

    spring security demo.

---
+++ security-web-demo/src/main/java/com/example/securitywebdemo/SecurityWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitywebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWebDemoApplication.class, args);
+	}
+
+}
