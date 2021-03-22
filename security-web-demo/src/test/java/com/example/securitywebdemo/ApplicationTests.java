a596ccad0563936e43b534f6151768e872457958
beginor <beginor@qq.com>
2021-03-22 17:56:21 +0800

    refact.

--- security-web-demo/src/test/java/com/example/securitywebdemo/ApplicationTests.java
+++ security-web-demo/src/test/java/com/example/securitywebdemo/ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SecurityWebDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
